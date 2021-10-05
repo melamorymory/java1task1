a4a46c17392809fbd0eb7b5d3c75e89e0c738337
Olga Lebedeva <birdswann@gmail.com>
2021-10-05 15:08:33 +0300

    Task 1 done

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 2_000_000_000;
+        int transitionSum = 500_000_000;
+        int total = account + transitionSum;
+        System.out.println(total);
+    }
+}
